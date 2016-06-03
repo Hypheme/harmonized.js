37276ed0be3d3f190bce4c8b4d8761df565a8e93
Christoph Auer <cauer@hyphe.me>
2016-06-03 13:22:34 +0200

    style(item): remove explanation comments, thanks @JohannesMerz

--- src/Item.js
+++ src/Item.js
@@ -65,10 +65,10 @@ export default class Item {
 
   save(values) {
     const autoSave = this.autoSave;
-    this.autoSave = true; // we want this.set to call _synchronize
-    const promise = this.set(values); // this task is async and we need this promise to be returned
-    this.autoSave = autoSave; // we want autoSave reset asap, therefore in the sync part
-    return promise; // now we return the promise
+    this.autoSave = true;
+    const promise = this.set(values);
+    this.autoSave = autoSave;
+    return promise;
   }
 
   set(values) {
