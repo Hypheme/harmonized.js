b9fa98fe6dc1264e157e68dc118fe6e4ab6bb043
Simon Jentsch <sjentsch@hyphe.me>
2016-10-03 22:57:39 +0200

    feat(PushQueue): add PushQueue class

--- src/BaseTransporter/PushQueue.js
+++ src/BaseTransporter/PushQueue.js
@@ -1,5 +1,27 @@
-export default class PushQueue {
-  constructor() {
+// @flow
+import TransporterMiddleware from '../TransporterMiddleware/TransporterMiddleware';
+import PushQueueItem from './PushQueueItem';
 
+export default class PushQueue {
+  _queue: Array<PushQueueItem> = [];
+
+  static createItem(action, payload) {
+    console.log('pushi', PushQueueItem);
+    return new PushQueueItem(action, payload);
+  }
+
+  getQueue(runtimeId: number) {
+    const queue = this._queue[runtimeId];
+    if (queue) {
+      return queue;
+    }
+
+    // When queue is not there, create a new queue
+    this._queue[runtimeId] = [];
+    return this._queue[runtimeId];
+  }
+
+  removeQueue(runtimeId: number) {
+    delete this._queue[runtimeId];
   }
 }
--- src/BaseTransporter/PushQueue.spec.js
+++ src/BaseTransporter/PushQueue.spec.js
@@ -1,1 +1,68 @@
+// const MockQueueItem = function MockQueueItem(action, payload) {
+//   console.log('should be called', action, payload);
+//   return { action, payload };
+// };
+
 import PushQueue from './PushQueue';
+
+describe('PushQueue', function () {
+  beforeEach(function () {
+    // console.log('wheee', PushQueueRewireApi);
+    // console.log('rerererewire', PushQueue.__Rewire__);
+    // console.log('rewire', PushQueueRewireApi.__Rewire__);
+    // PushQueue.__Rewire__('PushQueueItem', MockQueueItem);
+    this.queue = new PushQueue();
+  });
+
+  afterEach(function () {
+    // PushQueue.__ResetDependency__('PushQueueItem');
+  });
+
+  it('should get existing queue items for a given runtime ID', function () {
+    const expectedQueue = ['some', 'items'];
+    this.queue._queue = {
+      123: expectedQueue,
+    };
+
+    const returnedQueue = this.queue.getQueue(123);
+    expect(returnedQueue).toBe(expectedQueue);
+  });
+
+  it('should get a new queue when requesting a queue that is not there yet', function () {
+    this.queue._queue = {
+      123: ['some', 'other', 'queue'],
+    };
+
+    const returnedQueue = this.queue.getQueue(124);
+    expect(returnedQueue).toEqual([]);
+    expect(returnedQueue).toBe(this.queue._queue[124]);
+  });
+
+  it('should remove queue', function () {
+    this.queue._queue = {
+      122: ['some', 'queue'],
+      123: ['some', 'other', 'queue'],
+    };
+
+    this.queue.removeQueue(122);
+    expect(this.queue._queue).toEqual({
+      123: ['some', 'other', 'queue'],
+    });
+  });
+
+  xit('should create a new queue item', function () {
+    const createdItem = PushQueue.createItem('create', {
+      id: 123,
+      _id: 124,
+      payload: 'some',
+    });
+    expect(createdItem).toEqual({
+      action: 'create',
+      payload: {
+        id: 123,
+        _id: 124,
+        payload: 'some',
+      },
+    });
+  });
+});
