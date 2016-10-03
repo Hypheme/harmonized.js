3b0727aaf70ca1e8b5460d8948b212ad1c5375dd
Simon Jentsch <sjentsch@hyphe.me>
2016-10-04 01:40:50 +0200

    feat(BaseTransporter): add push methods and send middleware

--- src/BaseTransporter/BaseTransporter.js
+++ src/BaseTransporter/BaseTransporter.js
@@ -1,19 +1,79 @@
+// @flow
+import PushQueue from './PushQueue';
+import PushQueueItem from './PushQueueItem';
+import TransporterMiddleware from '../TransporterMiddleware/TransporterMiddleware';
+
 export default class BaseTransporter {
+  static middleware: TransporterMiddleware[] = [];
+  _queue: PushQueue;
 
-  create(item) {
-
+  constructor() {
+    this._queue = new PushQueue();
   }
 
-  update(item) {
-
+  create(item: Object) {
+    PushQueue.createItem('create', item);
   }
 
-  delete(item) {
+  update(item: Object) {
+    PushQueue.createItem('update', item);
+  }
 
+  delete(item: Object) {
+    PushQueue.createItem('delete', item);
   }
 
   push() {
+    const pushPromises = [];
+    for (const __id: string in this._queue._queue) {
+      if (this._queue._queue.hasOwnProperty(__id)) {
+        pushPromises.push(this.pushOne(__id));
+      }
+    }
 
+    return Promise.all(pushPromises);
+  }
+
+  _prepareSend(queueItem: PushQueueItem) {
+    throw new Error('should be implemented by the transporter');
+  }
+
+  _send(data: Object) {
+    throw new Error('should be implemented by the transporter');
+  }
+
+  _sendCurrentQueueItem(subQueue: PushQueueItem[], promise: Promise,
+    resolve: Function, reject: Function) {
+    const queueItem = subQueue[0];
+    queueItem.inProgress = true;
+    queueItem.promise = promise;
+    const preparedSendRequest = this._prepareSend(subQueue[0]);
+    this.constructor.runMiddleware('send', {
+      req: preparedSendRequest,
+      item: queueItem,
+    }).then(this._send.bind(this))
+    .then((res) => {
+      subQueue.shift();
+      if (subQueue.length === 0) {
+        resolve(res);
+      } else {
+        this._sendCurrentQueueItem(subQueue, promise, resolve, reject);
+      }
+    });
+  }
+
+  pushOne(__id: string) {
+    const subQueue = this._queue.getQueue(__id);
+    const queueItem = subQueue[0];
+    if (queueItem.inProgress) {
+      return queueItem.promise;
+    }
+
+    const promise = new Promise((resolve, reject) => {
+      this._sendCurrentQueueItem(subQueue, promise, resolve, reject);
+    });
+
+    return promise;
   }
 
   fetch() {
@@ -26,6 +86,21 @@ export default class BaseTransporter {
 
   initialFetch() {
 
+  }
+
+  static add(mw) {
+    this.middleware.push(mw);
+  }
+
+  static runMiddleware(subMiddleware, data) {
+    // go through middleware and chain then functions!
+    const middleware: Function[] = this.middleware.map((mw: Object) => mw[subMiddleware]);
+    let latestPromise = Promise.resolve(data);
+    for (const mw of middleware) {
+      latestPromise = latestPromise.then(mw);
+    }
+
+    return latestPromise;
   }
 
 }
