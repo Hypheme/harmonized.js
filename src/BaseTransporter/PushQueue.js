9d6694fa434a3cef3457679a24c0292c2b0ae358
Simon Jentsch <sjentsch@hyphe.me>
2016-10-03 16:10:56 +0200

    chore: add flow to dev dependencies

--- src/BaseTransporter/PushQueue.js
+++ src/BaseTransporter/PushQueue.js
@@ -1,3 +1,5 @@
 export default class PushQueue {
+  constructor() {
 
+  }
 }
--- src/TransporterMiddleware/TransporterMiddleware.js
+++ src/TransporterMiddleware/TransporterMiddleware.js
@@ -1,13 +1,14 @@
+// @flow
 import _ from 'lodash';
 
 export default class TransporterMiddleware {
-  // init: function() {},
-  // addItemToQueue: function(queueItem, queue) {},
-  // updateQueueAfterReceive: function(queueItem, originalItem, queue) {},
-  // send: function(req, item) {},
-  // receive: function(res, req, item) {},
-  // sendError: function(req, item) {},
-  // receiveError: function(res, req, item) {},
+  // init() {}
+  // addItemToQueue(queueItem, queue) {}
+  // updateQueueAfterReceive(queueItem, originalItem, queue) {}
+  // send(req, item) {}
+  // receive(res, req, item) {}
+  // sendError(req, item) {}
+  // receiveError(res, req, item) {}
 
   static methods = [
     'addItemToQueue',
@@ -18,9 +19,9 @@ export default class TransporterMiddleware {
     'receiveError',
   ];
 
-  static build() {
-    const newTransportMiddleware = new this(...arguments[0]);
-    const filteredMethods = Array.prototype.slice.call(arguments, 1);
+  static build(...args) {
+    const newTransportMiddleware: Object = new this(...args[0]);
+    const filteredMethods = args.slice(1);
     if (filteredMethods.length > 0) {
       _.difference(this.methods, filteredMethods).forEach((method) => {
         newTransportMiddleware[method] = undefined;
