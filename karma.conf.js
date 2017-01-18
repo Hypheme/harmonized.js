7d9ca57241e2036e1df2cd9d476c0acc4c9a1728
Simon Jentsch <sjentsch@hyphe.me>
2017-01-18 01:14:15 +0100

    remove spec files from coverage

--- karma.conf.js
+++ karma.conf.js
@@ -93,6 +93,7 @@ module.exports = function (config) {
     reporters: [
         { type: 'lcovonly', subdir: '.' },
         { type: 'json', subdir: '.' },
+        { type: 'text', subdir: '.' },
         // { type: 'html', subdir: '.' },
     ],
   };
