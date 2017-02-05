d7cb6e148e93419f2dab41d81a14aa2467fcda0e
Simon Jentsch <sjentsch@hyphe.me>
2017-02-05 16:26:08 +0100

    switch to use case based file structure

---
+++ test/integration/offline-all.js
@@ -0,0 +1,61 @@
+describe('all offline', function () {
+  describe('Store', function () {
+    xit('should be constructed and do initial fetch', function () {
+
+    });
+
+    xit('should create a new item', function () {
+
+    });
+
+    xit('should findOneOrFetch an item', function () {
+
+    });
+  });
+
+  describe('Item', function () {
+    xit('should be created from client storage', function () {
+
+    });
+
+    xit('should be created from trnasporter', function () {
+
+    });
+
+    xit('should be created from state', function () {
+
+    });
+
+    xit('should be updated from client storage', function () {
+
+    });
+
+    xit('should be updated from transporter', function () {
+
+    });
+
+    xit('should be updated from state', function () {
+
+    });
+
+    xit('should be deleted from client storage', function () {
+
+    });
+
+    xit('should be deleted from transporter', function () {
+
+    });
+
+    xit('should be deleted from state', function () {
+
+    });
+
+    xit('should fetch from client storage', function () {
+
+    });
+
+    xit('should fetch from transporter', function () {
+
+    });
+  });
+});
