a55c4d488189057265fc2536ec24916e136f73de
Simon Jentsch <sjentsch@hyphe.me>
2017-02-05 16:15:24 +0100

    add first empty test cases

---
+++ test/integration/Item.integration.spec.js
@@ -0,0 +1,45 @@
+describe('Item', function () {
+  it('should be created from client storage', function () {
+
+  });
+
+  it('should be created from trnasporter', function () {
+
+  });
+
+  it('should be created from state', function () {
+
+  });
+
+  it('should be updated from client storage', function () {
+
+  });
+
+  it('should be updated from transporter', function () {
+
+  });
+
+  it('should be updated from state', function () {
+
+  });
+
+  it('should be deleted from client storage', function () {
+
+  });
+
+  it('should be deleted from transporter', function () {
+
+  });
+
+  it('should be deleted from state', function () {
+
+  });
+
+  it('should fetch from client storage', function () {
+
+  });
+
+  it('should fetch from transporter', function () {
+
+  });
+});
---
+++ test/integration/Store.integration.spec.js
@@ -0,0 +1,13 @@
+describe('Store', function () {
+  xit('should be constructed and do initial fetch', function () {
+
+  });
+
+  xit('should create a new item', function () {
+
+  });
+
+  xit('should findOneOrFetch an item', function () {
+
+  });
+});
